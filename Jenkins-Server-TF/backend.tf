0b2268c7f75970507ea3fad4deaeaf5f2fad1f7c
SeanGuoAU <jcezz137@gmail.com>
2025-03-04 14:03:36 +1100

    test2

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "test1-tfstate-sean"
-    region         = "us-east-1"
+    bucket         = "my-ews-baket1"
+    region         = "ap-southeast-2"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
